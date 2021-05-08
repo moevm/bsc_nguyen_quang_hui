ffe127e4516b9cfade3739de1707da98e5f4fede
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2021-05-09 02:43:15 +0300

    added run script

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+## update git
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt update
+sudo apt install git
+
+## install git large file storage
+sudo apt install git-lfs
+git lfs install
+
+## update repo with models
+git lfs pull
+
+## get submodule code
+git submodule init
+git submodule update
+
+## launch sw-auto
+cd sw-auto
+git checkout mls_analysis_service
+
+cd SciencePaperAnalyzer
+
+echo "Building SW-Auto"
+docker-compose -f docker-compose-mlss.yml build
+
+echo "Running SW-Auto"
+docker-compose -f docker-compose-mlss.yml up -d
+
+## launch verificationapi
+cd ../../verificationapi
+
+echo "Building MLS Analysis Service"
+docker-compose -f docker-compose-sw-auto.yml build
+
+echo "Running MLS Analysis Service"
+docker-compose -f docker-compose-sw-auto.yml up -d
